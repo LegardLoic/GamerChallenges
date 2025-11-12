334464c7b3010f5c96f02a74ae9c3df8b218a082
LegardLoic <loic.legard@oclock.school>
2025-11-12 14:44:58 +0100

    🌟 feat(app): add store redux with all types for auth methode & init fetch/me on start
    Changelog: feature

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -4,7 +4,31 @@ import Footer from '../Footer/Footer'
 import HomePage from '../../pages/HomePage/HomePage'
 import { Routes, Route } from 'react-router'
 import LoginPage from '../../pages/LoginPage/LoginPage'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { getUserInfo } from '@/store/reducers/userReducer'
+import type { RootState, AppDispatch } from "@/store";
 export default function App (){
+    
+    const dispatch = useDispatch<AppDispatch>();
+    const { isAuth, userInfo, loading, error } = useSelector(
+        (store: RootState) => store.userStore
+    );
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                await dispatch(getUserInfo());
+            } catch (error) {
+                console.log("Authentication failed:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+    checkAuth();
+  }, [dispatch]);
     return (
         <Layout Header={<Header />} Footer={<Footer />}>
             <Routes>
