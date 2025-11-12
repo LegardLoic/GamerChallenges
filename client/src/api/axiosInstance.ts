56fc9e8ee890160a766ae5c4df8de7bd33a66fb6
LegardLoic <loic.legard@oclock.school>
2025-11-12 15:43:56 +0100

    🔨 modify(app): simplify axios interceptor
    Changelog: modify

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -6,31 +6,19 @@ export const axiosInstance = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-// 401 interceptor
+
 let refreshing = false;
-let queue: Array<() => void> = [];
 
-axiosInstance.interceptors.response.use(
-  (res) => res,
-  async (err) => {
-    if (err.response?.status !== 401 || err.config?._retry) {
-      return Promise.reject(err);
-    }
-    err.config._retry = true;
 
-    if (!refreshing) {
-      refreshing = true;
+axiosInstance.interceptors.response.use(undefined, async (error) => {
+  if (error.response?.status === 401 && !refreshing) {
+    refreshing = true;
       try {
         await axiosInstance.post("/auth/refresh");
-        queue.forEach((resume) => resume());
-        queue = [];
-        return axiosInstance(err.config);
+        return axiosInstance(error.config);
       } finally {
         refreshing = false;
       }
-    }
-    return new Promise((resolve) =>
-      queue.push(() => resolve(axiosInstance(err.config)))
-    );
   }
-);
+  throw error;
+});
